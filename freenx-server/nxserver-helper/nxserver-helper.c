53a979d11349eaf02899c9299e2779aeccbe8698
fabianx <fabianx@80778403-ebee-0310-8e0a-e86a689f3fff>
2008-03-11 05:55:27 +0000

    * Made the slave mode finally functional. With that slave mode it is  possible to do a single sign on instead of the multiple logins used  before. It is also possible to use a suid wrapper to login as user.  With single sign on session startup is a lot faster. This is true  especially if there are many printers and files to be shared. (fabianx@bat.berlios.de)
    This is also the basis for importing nxserver_suid from redesign.
    
    
    
    git-svn-id: svn+ssh://svn.berlios.de/svnroot/repos/freenx/trunk@517 80778403-ebee-0310-8e0a-e86a689f3fff

--- freenx-server/nxserver-helper/nxserver-helper.c
+++ freenx-server/nxserver-helper/nxserver-helper.c
@@ -10,12 +10,27 @@
 #include <sys/types.h>
 #include <sys/socket.h>
 #include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
 
 int main(int argc, char* argv[])
 {
 	int fds[2];
+	char buf[1024];
 
 	socketpair(AF_UNIX, SOCK_STREAM, 0, fds);
+	
+	// This fd can be used by the server for spawning the node
+	snprintf(buf, 1022, "%d", fds[1]);
+	setenv("NX_SERVER_FD", buf, 1);
+	
+	// This fd can be used for communication with the node
+	snprintf(buf, 1022, "%d", fds[0]);
+	setenv("NX_COMM_FD", buf, 1);
+
+	// We do not trust this user, he still has to login
+	unsetenv("NX_TRUSTED_USER");
+
 	argv++;
 	execv(argv[0], argv);
 }
